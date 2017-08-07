7f96ffe795d1ac4b6481204a47f13cef38d780cd
Rachel White <loveless@gmail.com>
2017-08-07 19:53:36 -0400

    got skeleton of changelog bot up and running

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
-var restify = require('restify');
-var builder = require('botbuilder');
+"use strict";
+const builder = require("botbuilder");
+const request = require("request");
+const hostInfo = require('./intents/hostInfo');
+const podcastSummary = require('./intents/podcastSummary');
+const config = require('./config');
 
-// Setup Restify Server
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
-});
+// swap out for (process.env.NODE_ENV == 'development')
+const useEmulator = true;
 
-// Create chat connector for communicating with the Bot Framework Service
-var connector = new builder.ChatConnector({
+const connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-// Listen for messages from users 
-server.post('/api/messages', connector.listen());
+const bot = new builder.UniversalBot(connector);
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
-});
+const LuisModelUrl = config.luisurl;
+
+// Main dialog with LUIS
+const recognizer = new builder.LuisRecognizer(LuisModelUrl);
+const intents = new builder.IntentDialog({ recognizers: [recognizer] })
+    .matches('hostInfo', hostInfo)
+    .matches('podcastSummary', podcastSummary)
+    .matches('greeting', (session, args) => {session.send('hello i am a friendly bot', session.message.text)})
+    .onDefault((session) => {session.send('Sorry, I did not understand \'%s\'.', session.message.text)});
+
+bot.dialog('/', intents);
+
+if (useEmulator) {
+    const restify = require('restify');
+    const server = restify.createServer();
+    server.listen(3978, function() {
+        console.log('test bot endpont at http://localhost:3978/api/messages');
+    });
+    server.post('/api/messages', connector.listen());    
+} else {
+    module.exports = { default: connector.listen() }
+}
+
---
+++ intents/nextEpisode.js
@@ -0,0 +1,51 @@
+var request = require("request");
+//var movieApiConfig = require('../movieApiConfig');
+
+module.exports = function (session, args) {
+  // const movieEntity = args.entities.filter((entity) => entity.type === 'movieTitle');
+  // const characterEntity = args.entities.filter((entity) => entity.type === 'characterName');
+
+  // const movieTitle = movieEntity.length ? movieEntity[0].entity : null;
+  // const characterName = characterEntity.length ? characterEntity[0].entity : null;
+
+
+  // const teststring = 'intent: actorForRole, movie title: ' + movieTitle + ', character name: ' + characterName;
+  
+  session.send('you asked for information on the next episode');
+
+  // const movieSearchUrl = movieApiConfig.movieUrl + 'search/movie/?query=' + movieTitle + '&api_key=' + movieApiConfig.movieKey;
+
+  // request(movieSearchUrl, (error, response, body) => {
+  //   if (!error && response.statusCode == 200) {
+
+  //     const results = JSON.parse(body).results;
+
+  //     if (!results.length) {
+  //       return session.send("I couldn't find the right movie you're asking about.");
+  //     }
+
+  //     // this is a basic placeholder, we should return a set of movies to choose from as chat buttons
+  //     // if (results.length > 1) {
+  //     //     return session.send("I got multiple results, I'm not sure which one to choose.");
+  //     // }
+
+  //     const movieId = results[0].id;
+  //     const movieCreditsUrl = movieApiConfig.movieUrl + 'movie/' + movieId + '/credits?api_key=' + movieApiConfig.movieKey;
+
+  //     request(movieCreditsUrl, (error, response, body) => {
+  //       if (!error && response.statusCode == 200) {
+  //         const findCharacter = JSON.parse(body).cast.filter((c) => c.character.toLowerCase().includes(characterName));
+  //         const character = findCharacter.length ? findCharacter[0] : null;
+  //         if (!character) {
+  //           return session.send("Sorry, I didn't find that character in the movie.");
+  //         }
+  //         session.send("I think the person you're looking for is " + character.name);
+  //       } else {
+  //         session.send("Sorry, something went wrong when I was trying to look up the character in the movie.");
+  //       }
+  //     });
+  //   } else {
+  //     session.send("Sorry, something went wrong when I was looking up the movie.")
+  //   }
+  // });
+};
