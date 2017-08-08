0d75ffc4ca4ba38fae7d39018ecc96eba2cc28b1
Rachel White <loveless@gmail.com>
2017-08-08 17:38:51 -0400

    got db query working for name

--- intents/hostInfo.js
+++ intents/hostInfo.js
@@ -4,24 +4,43 @@ const client = new pg.Client({
   host: config.HOST,
   port: config.PORT,
   user: config.USER,
+  database: config.DATABASE,
   password: config.PASSWORD,
+  ssl: false
 })
 
 
 module.exports = function (session, args) {
   const hostEntity = args.entities.filter((entity) => entity.type === 'podcastHost');
-  const hostName = hostEntity.length ? hostEntity[0].entity : null;
-  
-  console.log(hostName);
-  console.log('---------');
+  let hostName = hostEntity.length ? hostEntity[0].entity : null;
 
-  ///client.connect()
+  function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
+  };
 
-  client.query('SELECT name FROM people', function (err, results) {
-    console.log(err);
-    console.log('~~~~~~~~~~');
-    console.log(results);
-  })
+  hostName = toTitleCase(hostName);
+
+  console.log('|' + hostName + '|');
+
+
+  const query = {
+    name: 'find-user',
+    text: 'SELECT name FROM people WHERE name = $1',
+    values: [hostName]
+  };
+
+  client.connect((err) => {
+    if (err) {
+      throw err;
+    }
+    client.query(query, (err, res) => {
+      if (err) {
+        console.log(err.stack)
+      } else {
+        console.log(res.rows[0])
+      }
+    });
+  });
 
   session.send('you asked for information on a host');
 
